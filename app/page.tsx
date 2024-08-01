4435ea6b395b52040bce977be782dcc5f0da0c9e
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-08-01 14:05:06 -0700

    started navbar sidebar and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
+import Dashboard from "@/components/Dashboard";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
-    </main>
+    <Dashboard />
   );
 }
