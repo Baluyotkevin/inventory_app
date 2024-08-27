31f2fd8a2473e6f15cd49404a5f8df69d857bdf5
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-08-27 14:45:01 -0700

    finished products page and create products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import Header from '@/components/Header';
-import { useGetProductsQuery } from '@/state/api';
-import { PlusCircle, PlusCircleIcon, SearchIcon } from 'lucide-react';
+import Rating from '@/components/Rating';
+import { useCreateProductMutation, useGetProductsQuery } from '@/state/api';
+import { PlusCircleIcon, SearchIcon } from 'lucide-react';
 import { useState } from 'react';
+import CreateProductModal from './CreateProductModal';
+
+type ProductFormData = {
+    name: string;
+    price: number;
+    stockQuantity: number;
+    rating: number;
+}
 
 const Products = () => {
     const [searchTerm, setSearchTerm] = useState("");
@@ -14,8 +23,13 @@ const Products = () => {
         isError
     } = useGetProductsQuery(searchTerm);
 
+    const [createProduct] = useCreateProductMutation();
+    const handleCreateProduct = async (productData: ProductFormData) => {
+        await createProduct(productData);
+    }
+
     if (isLoading) return <div className="py-4">Loading...</div>
-    if (isError || products) return <div className="text-center text-red-500 py-4">Failed to fetch products</div>
+    if (isError || !products) return <div className="text-center text-red-500 py-4">Failed to fetch products</div>
     
   return (
     <div className="mx-auto pb-5 w-full">
@@ -34,13 +48,45 @@ const Products = () => {
         <div className="flex justify-between items-center mb-6">
             <Header name="Products" />
             <button
-                className="flex items-center bg-blue-100 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+                className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
                 onClick={() => setIsModalOpen(true)}
             >
                 <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
                 Product
             </button>
         </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-between">
+            {isLoading ? (
+                <div>Loading</div>
+            ) : (
+                 products?.map((product) => (
+                    <div key={product.productId} className="border shadow rounded-md p-4 max-w-full w-full mx-auto">
+                        <div className="flex flex-col items-center">
+                            img
+                            <h3 className="text-lg text-gray-900 font-semibold">
+                                {product.name}
+                            </h3>
+                            <p className="text-gray-800">${product.price.toFixed(2)}</p>
+                            <div className="text-sm text-gray-600 mt-1">
+                                Stock: {product.stockQuantity}
+                            </div>
+                            {product.rating && (
+                                <div className="flex items-center mt-2">
+                                    <Rating rating={product.rating} />
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                 ))
+            )}
+        </div>
+
+        <CreateProductModal 
+            isOpen={isModalOpen} 
+            onClose={() => setIsModalOpen(false)} 
+            onCreate={handleCreateProduct}
+        />
     </div>
   )
 }
