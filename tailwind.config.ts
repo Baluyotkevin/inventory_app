36374425297e0758fcaafb710a8c40b94197ff36
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-09-02 09:16:43 -0700

    fixed light/dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,33 +10,33 @@ const baseColors = [
   "blue",
   "indigo",
   "purple",
-  "pink"
-]
+  "pink",
+];
 
 const shadeMapping = {
   "50": "900",
   "100": "800",
   "200": "700",
-  "300": " 600",
+  "300": "600",
   "400": "500",
   "500": "400",
   "600": "300",
   "700": "200",
   "800": "100",
-  "900": "50"
-}
+  "900": "50",
+};
 
 const generateThemeObject = (colors: any, mapping: any, invert = false) => {
   const theme: any = {};
   baseColors.forEach((color) => {
-    theme[color] = {}
+    theme[color] = {};
     Object.entries(mapping).forEach(([key, value]: any) => {
       const shadeKey = invert ? value : key;
-      theme[color][key] = colors[color][shadeKey]
+      theme[color][key] = colors[color][shadeKey];
     });
   });
-  return theme
-}
+  return theme;
+};
 
 const lightTheme = generateThemeObject(colors, shadeMapping);
 const darkTheme = generateThemeObject(colors, shadeMapping, true);
@@ -44,14 +44,14 @@ const darkTheme = generateThemeObject(colors, shadeMapping, true);
 const themes = {
   light: {
     ...lightTheme,
-    white: "#fffff"
+    white: "#ffffff",
   },
   dark: {
     ...darkTheme,
     white: colors.gray["950"],
-    black: colors.gray["50"]
-  }
-}
+    black: colors.gray["50"],
+  },
+};
 
 const config: Config = {
   darkMode: "class",
